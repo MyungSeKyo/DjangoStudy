5300eac5830fa6c17887f11ff7a1c1532643bb4c
oliver <oliver@kidsnote.com>
2018-02-28 18:50:38 +0900

    first page added

--- MySite/urls.py
+++ MySite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from MySite.views import HomeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('admin/', admin.site.urls),
 ]
