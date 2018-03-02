cb716e3014c95032e625463efe5a9e02d7f80103
oliver <oliver@kidsnote.com>
2018-03-02 16:40:28 +0900

    base.html

--- MySite/settings.py
+++ MySite/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'MySite/../static'), ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
