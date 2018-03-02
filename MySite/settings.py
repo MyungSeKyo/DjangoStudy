29b24704ca65232c9abadf916678109036a42728
oliver <oliver@kidsnote.com>
2018-03-02 18:19:16 +0900

    base.css

--- MySite/settings.py
+++ MySite/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'MySite/../static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
